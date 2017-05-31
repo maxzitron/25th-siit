e5a9a0b7e7f9b855c30dcb1289d69599191363e9
Max <maxzitron@gmail.com>
2017-05-31 21:46:30 +1000

    Added croppie

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,53 @@
 /**
  * Created by max on 31/5/17.
  */
+$(document).ready(function () {
+    var $cropper;
+
+
+    function readFile(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.upload-demo').addClass('ready');
+                $cropper.croppie('bind', {
+                    url: e.target.result
+                }).then(function () {
+                    console.log('jQuery bind complete');
+                });
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    // initiate cropper
+    $cropper = $('.cropper').croppie({
+        enableExif: true,
+        viewport: {
+            width: 280,
+            height: 280,
+        },
+        boundary: {
+            width: 300,
+            height: 300
+        }
+    });
+
+    // trigger upload button
+    $('.uplopad-btn').click(function (e) {
+        e.preventDefault();
+        $('#upload').trigger("click");
+    })
+
+    $('#upload').change(function() {
+        $('.cropper').removeClass('hidden');
+        $('.uplopad-btn').text("New Image");
+        readFile(this);
+    });
+
+
+
+
+});
