6febf254ef4a6b6dc759cb486510943784604fd1
Max <maxzitron@gmail.com>
2017-05-31 23:19:09 +1000

    Added frame

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@
  */
 $(document).ready(function () {
     var $cropper;
-
+    var $size = 1024;
 
     function readFile(input) {
         if (input.files && input.files[0]) {
@@ -42,11 +42,50 @@ $(document).ready(function () {
     })
 
     $('#upload').change(function() {
-        $('.cropper, .download-btn').removeClass('hidden');
+        $("#result").addClass("hidden");
+        $('.cropper, .editor, .download-btn').removeClass('hidden');
         $('.uplopad-btn').text("New Image");
+
         readFile(this);
     });
 
+    $('.download-btn').click(function () {
+        $(this).addClass("hidden");
+
+        $cropper.croppie('result', {
+            type: 'canvas',
+            size: { width: $size, height: $size },
+            format: 'png'
+        }).then(function (response) {
+            $(".editor").addClass("hidden");
+            render({
+                src: response
+            });
+        });
+    });
+
+
+    function render(result) {
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext("2d");
+
+        var image = new Image();
+        image.src = result.src;
+        image.onload = function() {
+            var imageOverlay = new Image();
+            imageOverlay.src = 'assets/frame.png';
+
+            imageOverlay.onload = function() {
+                context.fillStyle = 'white';
+                context.fillRect(0, 0, $size, $size);
+                context.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width, image.height);
+                context.drawImage(imageOverlay, 0, 0, imageOverlay.width, imageOverlay.height, 0, 0, imageOverlay.width, imageOverlay.height);
+                var imgURL = canvas.toDataURL("image/jpeg");
+                $("#result-img").attr("src", imgURL);
+                $("#result").removeClass("hidden");
+            };
+        };
+    }
 
 
 
